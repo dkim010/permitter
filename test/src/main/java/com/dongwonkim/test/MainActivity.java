d9d10fff0a65fc07d97f74b11d8f1f6dce7982ed
Dongwon Kim <dongwon.kim@navercorp.com>
2016-09-18 16:47:03 +0900

    connect the online library through github & jitpack
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- test/src/main/java/com/dongwonkim/test/MainActivity.java
+++ test/src/main/java/com/dongwonkim/test/MainActivity.java
@@ -2,15 +2,12 @@ package com.dongwonkim.test;
 
 import android.Manifest;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.dongwonkim.permitter.OnPermissionResult;
 import com.dongwonkim.permitter.Permitter;
